550d0744e55bed83d318bb605ed5a460c9b4234f
Kevin Morais <kevin.morais@liivetouch.com>
2020-12-24 14:39:01 +0100

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "StayTuned",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "StayTuned",
+            targets: ["StayTuned"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "StayTuned",
+            path: "https://github.com/StayTunedAds/staytuned-ios-sdk/releases/download/1.0/StayTunedSDK.xcframework.zip"
+        )
+    ]
+)
